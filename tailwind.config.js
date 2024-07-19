6b4d98a7c0dc592d3e0700b39a648b61fa4eb544
M-Panz <expectotype@gmail.com>
2024-07-19 17:21:09 +0700

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/ @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./files/js//.js",
+    "./files/svg/**/.html",
+    "./class//*.{html,js,php}",
+    "./include//*.{html,js,php}",
+    "./index.html",
+  ],
+  theme: {
+    fontFamily: {
+      SFPRODISPLAY: ['SFPRODISPLAY', 'sans-serif'],
+      GeneralSans: ['GeneralSans', 'sans-serif'],
+      ClashGrotesk: ['Clash-Grotesk', 'sans-serif'],
+    },
+    screens: {
+      's': '320px',
+      'ss': '360px',
+      'xs': '420px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px',
+      '3xl': '1536px',
+      '4xl': '1920px',
+    },
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        PrimRed: '#FF6A49',
+        Primblue: {
+          300: "#DDEFFF",
+          500: "#2B99FF",
+          800: "#141E4F",
+        },
+        OddyNeutral: {
+          100: "#FFFFFF",
+          200: "#F5F5F5",
+          300: "#E5E5E5",
+          400: "#D4D4D4",
+          500: "#A3A3A3",
+          600: "#737373",
+          700: "#525252",
+          800: "#404040",
+          900: "#262626",
+          1000: "#171717",
+        }
+      },
+    },
+  },
+  plugins: [],
+}
